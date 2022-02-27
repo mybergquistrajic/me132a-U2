f5c1e0f16d094ee3318d929d83935756053de47c
My Bergquist Rajic <mybergqvist@gmail.com>
2022-02-27 14:04:29 +0100

    Filled database

--- database.js
+++ database.js
@@ -1,2 +1,74 @@
 "use strict"
 
+let database = [
+    {
+        id: 1,
+        country: "Italy",
+        artist: "Måneskin",
+        song: "Zitti e Buoni",
+        year: 2021,
+    },
+    {
+        id: 2,
+        country: "The Netherlands",
+        artist: "Teach-In",
+        song: "Ding-A-Dong",
+        year: 1975,
+    },
+    {
+        id: 3,
+        country: "Sweden",
+        artist: "Loreen",
+        song: "Euporia",
+        year: 2012,
+    },
+    {
+        id: 4,
+        country: "Norway",
+        artist: "Alexander Rybak",
+        song: "Fairytale",
+        year: 2009,
+    },
+    {
+        id: 5,
+        country: "Germany",
+        artist: "Lena",
+        song: "Satellite",
+        year: 2010,
+    },
+    {
+        id: 6,
+        country: "Sweden",
+        artist: "ABBA",
+        song: "Waterloo",
+        year: 1974,
+    },
+    {
+        id: 7,
+        country: "Ireland",
+        artist: "Johnny Logan",
+        song: "What's Another Year",
+        year: 1980,
+    },
+    {
+        id: 8,
+        country: "Ireland",
+        artist: "Johnny Logan",
+        song: "Hold Me Now",
+        year: 1987,
+    },
+    {
+        id: 9,
+        country: "Finland",
+        artist: "Lordi",
+        song: "Hard Rock Hallelujah",
+        year: 2006,
+    },
+    {
+        id: 10,
+        country: "Israel",
+        artist: "Milk and Honey",
+        song: "Hallelujah",
+        year: 1979,
+    },
+]
